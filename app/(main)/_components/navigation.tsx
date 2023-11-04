04b9c328dc33a97a5f3e81bb958d8d8846809c4a
bharath-dc <149763884+bharath-dc@users.noreply.github.com>
2023-11-04 09:06:59 +0000

    basic db setup working

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -6,10 +6,12 @@ import { usePathname } from "next/navigation";
 import React, { useRef, ElementRef, useState, useEffect } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { UserItem } from "./user-item";
+import { useQuery } from "convex/react";
+import {api} from "@/convex/_generated/api"
 const Navigation = () => {
     const pathname = usePathname();
     const isMobile = useMediaQuery("(max-width: 768px)")
-    
+    const documents = useQuery(api.documents.get)
     const isResizingRef = useRef(false)
     const sidebarRef = useRef<ElementRef<"aside">>(null);
     const navbarRef = useRef<ElementRef<"div">>(null);
@@ -109,11 +111,14 @@ const Navigation = () => {
                     <ChevronsLeft className="h-6 w-6"/>
                 </div>
                 <div>
-                    {/* <p>Action Items</p> */}
                     <UserItem/>
                 </div>
                 <div className="mt-4">
-                    <p>Documents</p>
+                    {documents?.map((document) => (
+                        <p key={document._id}>
+                            {document.title}
+                        </p>
+                    ))}
                 </div>
                 <div
                     onMouseDown={handleMouseDown}
